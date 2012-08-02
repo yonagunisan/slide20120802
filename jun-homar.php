a9ec96962fe8a08cd901fee33b7340976b7c4786
yonaguni <t07809hm@sfc.keio.ac.jp>
2012-08-02 12:03:47 +0900

    Update jun-homar.php

--- jun-homar.php
+++ jun-homar.php
@@ -1,7 +1,8 @@
 <div id="jh-1" class="step test" data-x="-5000" data-y="0" data-scale="2">
-  <p>This is sample for aa.</p>
-  <ul><li>hoge</li><li>hoge</li></ul>
+  <p>Tutorial</p>
+  <ul><li>Startup</li><li>Tips</li><li>TO/GA Settings</li></ul>
 </div>
 <div id="jh-2" class="step test" data-x="-3000" data-y="0" data-scale="2">
-  <p>This is 2nd sample for aa.</p>
+  <p>Startup</p>
+  <ul><li>新しいプロジェクトを使用可能にする</li><li>TO/GAに入る</li><li>フォルダーの編集方法</li><li>ファイルの編集方法</li><li>コードの編集方法</li><li>実行画面を表示する</li></ul>
 </div>
